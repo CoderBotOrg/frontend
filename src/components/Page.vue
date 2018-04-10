b6fcaeae27188c35403473b57ed6239791e2c437
Antonio Vivace <puma95.95@gmail.com>
2018-04-10 12:35:27 +0200

    base vuetify layout

---
+++ src/components/Page.vue
@@ -0,0 +1,72 @@
+<template>
+<div id="app">
+  <v-app id="inspire">
+    <v-navigation-drawer
+      fixed
+      v-model="drawer"
+      app
+    >
+      <v-list dense>
+        <v-list-tile @click="">
+          <v-list-tile-action>
+            <v-icon>home</v-icon>
+          </v-list-tile-action>
+          <v-list-tile-content>
+            <v-list-tile-title>Home</v-list-tile-title>
+          </v-list-tile-content>
+        </v-list-tile>
+        <v-list-tile @click="">
+          <v-list-tile-action>
+            <v-icon>contact_mail</v-icon>
+          </v-list-tile-action>
+          <v-list-tile-content>
+            <v-list-tile-title>Contact</v-list-tile-title>
+          </v-list-tile-content>
+        </v-list-tile>
+      </v-list>
+    </v-navigation-drawer>
+    <v-toolbar color="indigo" dark fixed app>
+      <v-toolbar-side-icon @click.stop="drawer = !drawer"></v-toolbar-side-icon>
+      <v-toolbar-title>Application</v-toolbar-title>
+    </v-toolbar>
+    <v-content>
+      <v-container fluid fill-height>
+        <v-layout
+          justify-center
+          align-center
+        >
+          <v-flex text-xs-center>
+            <v-tooltip left>
+              <v-btn icon large :href="source" target="_blank" slot="activator">
+                <v-icon large>code</v-icon>
+              </v-btn>
+              <span>Source</span>
+            </v-tooltip>
+          </v-flex>
+        </v-layout>
+      </v-container>
+    </v-content>
+    <v-footer color="indigo" app inset>
+      <span class="white--text">&copy; 2017</span>
+    </v-footer>
+  </v-app>
+</div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  data() {
+    return {
+      drawer: null,
+      source: null,
+      msg: 'Welcome to Your Vue.js App',
+    };
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
--- src/main.js
+++ src/main.js
@@ -1,15 +1,36 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+// Vue base
 import Vue from 'vue';
-import App from './App';
-import router from './router';
+import VueRouter from 'vue-router';
 
+import axios from 'axios';
+import Vuetify from 'vuetify';
+import 'vuetify/dist/vuetify.min.css';
+
+// Components
+import App from './App';
+import page from './components/Page';
+
+// Configuration
+Vue.use(VueRouter);
+Vue.use(Vuetify);
+Vue.prototype.$axios = axios.create();
 Vue.config.productionTip = false;
+
+// Routing
+const routes = [
+  { path: '/', component: page },
+];
+
+const router = new VueRouter({
+  routes, // short for `routes: routes`
+});
+
 
 /* eslint-disable no-new */
 new Vue({
-  el: '#app',
   router,
-  components: { App },
-  template: '<App/>',
+  el: '#app',
+  render: h => h(App),
 });
