98df50835238348726271f8f5e67af28e5276d94
Antonio Vivace <avivace4@gmail.com>
2018-08-28 23:26:29 +0200

    Move static Blockly dependencies to own folder. Add locales and custom blocks

--- src/components/BlocklyTest.vue
+++ src/components/BlocklyTest.vue
@@ -1,5 +1,5 @@
-<template> 
- <div class="hello"> 
+<template>
+ <div class="hello">
  	aa
    <div ref="blocklyDiv" id="blocklyDiv" style="height: 480px; width: 600px;"></div>
 </div>
@@ -8,7 +8,7 @@
 
 
 <script>
-// var workspace = Blockly.inject('blocklyDiv', {toolbox: this.$refs.toolbox}); 
+// var workspace = Blockly.inject('blocklyDiv', {toolbox: this.$refs.toolbox});
 
 export default {
   name: 'BlocklyTest',
