fbf57fdc0e24c7e0aacc3d8a38e48f27bce7afdc
Antonio Vivace <avivace4@gmail.com>
2018-09-02 00:53:03 +0200

    Clean and comment toolbox XML decoding. Move toolbox definition from data() to mounted().

--- src/components/BlocklyTest.vue
+++ src/components/BlocklyTest.vue
@@ -1,29 +1,38 @@
 <template>
- <div class="hello">
- 	aa
-   <div ref="blocklyDiv" id="blocklyDiv" style="height: 480px; width: 600px;"></div>
-</div>
+	<div>
+		<div ref="blocklyDiv" id="blocklyDiv" style="height: 480px; width: 600px;">
+		</div>
+	</div>
 </template>
 
 
 
 <script>
-// var workspace = Blockly.inject('blocklyDiv', {toolbox: this.$refs.toolbox});
-
 export default {
-  name: 'BlocklyTest',
-  data() {
-    return {
-      toolbox: require('../assets/toolbox_advanced.xml'),
-    };
-  },
-  mounted() {
+	name: 'Blockly',
+	data() {
+		return {
+			test: 'a',
+		};
+	},
+	mounted() {
+		// Should be dependent on the chosen toolbox,
+		//  possibly an AJAX call if it's a custom toolbox (?)
+		var toolbox = require('../assets/toolbox_advanced.xml')
+		// Clean the base64 encoding of the resource, removing meta infos
+		var b64Toolbox = toolbox.substring(21).toString()
+		// Decode it and get the clean serialized XML as plain string
+		var serializedToolbox = this.$base64.decode(b64Toolbox)
+		
+		// Initialise Blockly Instance
+		var workspace = Blockly.inject(
+			// Blockly container
+			this.$refs.blocklyDiv,
+			// Options
+			{
+				toolbox:serializedToolbox
+			});
 
-    var b64Toolbox = this.toolbox.substring(21).toString()
-    var serializedToolbox = this.$base64.decode(b64Toolbox)
-    
-  	var workspace = Blockly.inject(this.$refs.blocklyDiv, {toolbox:serializedToolbox});
-
-  }
+	}
 };
 </script>
