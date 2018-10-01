2ca9e8d525152503245aaf4c4338808d88a9afe5
Antonio Vivace <avivace4@gmail.com>
2018-10-01 11:40:32 +0200

    API endpoint env variable

--- config/dev.env.js
+++ config/dev.env.js
@@ -3,5 +3,8 @@ const merge = require('webpack-merge')
 const prodEnv = require('./prod.env')
 
 module.exports = merge(prodEnv, {
-  NODE_ENV: '"development"'
+  NODE_ENV: '"development"',
+  // BASE API ENDPOINT, must point to the exposed Flask instance
+  CB_ENDPOINT: '"http://localhost:5000"'
+  // TODO: on production, should be '""'
 })
