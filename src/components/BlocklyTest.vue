255939c5abe4b707dd63dea84d1fc5956a687685
Antonio Vivace <avivace4@gmail.com>
2018-10-01 12:52:00 +0200

    Move Navbar and Vuetify layout in global component, injecting (routed) single component in the container

--- src/components/BlocklyTest.vue
+++ src/components/BlocklyTest.vue
@@ -15,7 +15,7 @@ export default {
 	name: 'Blockly',
 	data() {
 		return {
-			a: '',
+			a: ''
 		};
 	},
 	methods : {
--- src/components/Page.vue
+++ src/components/Page.vue
@@ -1,85 +1,5 @@
 <template>
-
-  <v-app id="inspire">
-    <v-navigation-drawer
-      fixed
-      v-model="drawer"
-      app
-    >
-      <v-list dense>
-        <v-list-tile>
-          <v-list-tile-action>
-            <v-icon>home</v-icon>
-          </v-list-tile-action>
-          <v-list-tile-content>
-            <v-list-tile-title>Home</v-list-tile-title>
-          </v-list-tile-content>
-        </v-list-tile>
-
-        Utente
-        <v-list-tile>
-          <v-list-tile-action>
-            <v-icon>account_box</v-icon>
-          </v-list-tile-action>
-          <v-list-tile-content>
-            <v-list-tile-title>Luigi Beretta</v-list-tile-title>
-          </v-list-tile-content>
-        </v-list-tile>
-
-        <v-list-tile>
-          <v-list-tile-action>
-            <v-icon>exit_to_app</v-icon>
-          </v-list-tile-action>
-          <v-list-tile-content>
-            <v-list-tile-title>Logout</v-list-tile-title>
-          </v-list-tile-content>
-        </v-list-tile>
-
-        Attività
-        <v-list-tile>
-          <v-list-tile-action>
-            <v-icon>add</v-icon>
-          </v-list-tile-action>
-          <v-list-tile-content>
-            <v-list-tile-title>Nuova</v-list-tile-title>
-          </v-list-tile-content>
-        </v-list-tile>
-
-        <v-list-tile>
-          <v-list-tile-action>
-            <v-icon>open_in_new</v-icon>
-          </v-list-tile-action>
-          <v-list-tile-content>
-            <v-list-tile-title>Apri</v-list-tile-title>
-          </v-list-tile-content>
-        </v-list-tile>
-
-        <v-list-tile>
-          <v-list-tile-action>
-            <v-icon>close</v-icon>
-          </v-list-tile-action>
-          <v-list-tile-content>
-            <v-list-tile-title>Chiudi</v-list-tile-title>
-          </v-list-tile-content>
-        </v-list-tile>
-
-        Altro
-        <v-list-tile>
-          <v-list-tile-action>
-            <v-icon>settings</v-icon>
-          </v-list-tile-action>
-          <v-list-tile-content>
-            <v-list-tile-title>Impostazioni</v-list-tile-title>
-          </v-list-tile-content>
-        </v-list-tile>
-
-      </v-list>
-    </v-navigation-drawer>
-    <v-toolbar color="indigo" dark fixed app>
-      <v-toolbar-side-icon @click.stop="drawer = !drawer"></v-toolbar-side-icon>
-      <v-toolbar-title>Coderbot</v-toolbar-title>
-    </v-toolbar>
-    <v-content>
+	<div>
       <p class="display-3"> Benvenuto in CoderBot </p>
       <p class="display-1"> Scegli un'azione per iniziare </p>
 
@@ -99,9 +19,7 @@
           </v-flex>
         </v-layout>
       </v-container>
-    </v-content>
-  </v-app>
-
+    </div>
 </template>
 <script>
 export default {
