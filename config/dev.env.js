a56fdeba2b91d5072cbedde04fcb6eac683708c5
Antonio Vivace <avivace4@gmail.com>
2018-10-01 12:48:56 +0200

    Separate v1 and v2 API endpoints

--- config/dev.env.js
+++ config/dev.env.js
@@ -4,7 +4,10 @@ const prodEnv = require('./prod.env')
 
 module.exports = merge(prodEnv, {
   NODE_ENV: '"development"',
-  // BASE API ENDPOINT, must point to the exposed Flask instance
-  CB_ENDPOINT: '"http://localhost:5000"'
+  // BASE API ENDPOINTs, must point to the exposed Flask instance
+  // legacy APIs
+  CB_ENDPOINT_v1: '"http://localhost:5000"'
+  // New Connexion API
+  CB_ENDPOINT_v2: '"http://localhost:5000/v2"'
   // TODO: on production, should be '""'
 })
