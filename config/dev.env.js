067808df6d10d4ccf552e0a342b2124102587166
Antonio Vivace <avivace4@gmail.com>
2018-10-10 15:57:05 +0200

    Document how ENDPOINT and APIv1 / v2 env variables are used (build config)

--- config/dev.env.js
+++ config/dev.env.js
@@ -2,12 +2,17 @@
 const merge = require('webpack-merge')
 const prodEnv = require('./prod.env')
 
+// Beware, changes in this files require a new build. 
+//  I.e. `npm run dev` is not aware of changes made here.
+
 module.exports = merge(prodEnv, {
   NODE_ENV: '"development"',
-  // BASE API ENDPOINTs, must point to the exposed Flask instance
-  // legacy APIs
-  CB_ENDPOINT_v1: '"http://localhost:5000"',
-  // New Connexion API
-  CB_ENDPOINT_v2: '"http://localhost:5000/v2"'
-  // TODO: on production, should be '""'
+  // Backend location. Must point to the exposed Flask instance
+  //  By default, if you run the backend locally, it's available at localhost:5000
+  //  If you run the Vue app in production, served by the Flask backend, it should be '""'
+  CB_ENDPOINT: '"http://localhost:5000"'
+  // Legacy APIs
+  APIv1: '""',
+  // New API, exposed by Connexion
+  APIv2: '"/v2"'
 })
