e736722d97c0ad00936bcca14c03b62c85f9e720
Antonio Vivace <avivace4@gmail.com>
2018-10-12 16:21:35 +0200

    Activity data, Typography and Language sections in new Activity

--- src/components/Activity.vue
+++ src/components/Activity.vue
@@ -80,20 +80,38 @@
 					<v-layout row wrap>
 						<!-- Column A -->
 						<v-flex xs12 md6>
-							<h2 class="text-xs-left">Dati Progetto </h2>
+							<h3 class="text-xs-left">Dati Attività </h3>
 							<v-form v-model="valid">
 								<v-text-field v-model="name" label="Nome" required></v-text-field>
 								<v-text-field v-model="description" label="Descrizione"></v-text-field>
 							</v-form>
 							<v-select v-model="select" :items="viste" label="Vista predefinita" required></v-select>
-							<h2 class="text-xs-left">Tipografia </h2>
+							<h3 class="text-xs-left">Tipografia </h3>
+							<v-slider :label="'Grandezza testo'" v-model="fontSize" :tick-labels="fontSizeLabels" :max="3" step="1" ticks="always" tick-size="3"></v-slider>
+							<v-switch :label="`Solo maiuscole`" v-model="capsSwitch"></v-switch>
+							<v-switch :label="`Modalità Daltonici`" v-model="daltonicSwitch"></v-switch>
+							<v-select v-if="daltonicSwitch" v-model="daltonic" :items="daltonicModes" label="Tipo daltonismo" required></v-select>
+							<v-radio-group v-model="bodyFont" column>
+								Carattere tipografico dell'interfaccia<br><br>
+								<v-radio label="Roboto" value="Roboto"></v-radio>
+								<v-radio label="Open Sans" value="OpenSans"></v-radio>
+								<v-radio label="Test Me (Altà leggibilità, indicato per dislessia)" value="testme"></v-radio>
+								<v-radio label="Open-Dyslexic (Altà leggibilità, indicato per dislessia)" value="open-dys"></v-radio>
+							</v-radio-group>
+							<v-radio-group v-model="codeFont" column>
+								Carattere tipografico del codice (valori, editor di codice)<br><br>
+								<v-radio label="Iosevka" value="iosevka"></v-radio>
+								<v-radio label="Inconsolata" value="inconsolata"></v-radio>
+							</v-radio-group>
 						</v-flex>
 						<!-- Column B -->
 						<v-flex xs12 md6>
+							<h3 class="text-xs-left">Lingua</h3>
+							<v-select v-model="uiLang" :items="langs" label="Lingua Interfaccia" required></v-select>
+							<v-select v-model="blocklyLang" :items="langs" label="Lingua Blocchi" required></v-select>
 						</v-flex>
 					</v-layout>
 				</v-container>
-			</v-content>
 			</v-content>
 		</v-app>
 	</div>
@@ -103,6 +121,30 @@ export default {
 	name: 'HelloWorld',
 	data() {
 		return {
+			value: 0,
+			fontSize: 1,
+			fontSizeLabels: [
+				'Piccolo',
+				'Medio',
+				'Grande',
+				'Molto grande'
+			],
+			capsSwitch: true,
+			daltonicSwitch: 0,
+			daltonicType: 1,
+			daltonicModes: [
+				'Deuteranomaly',
+				'Protanomaly',
+				'Protanopia'
+			],
+			bodyFont: "Roboto",
+			codeFont: "iosevka",
+			langs: [
+				'Italiano',
+				'Inglese'
+			],
+			uiLang: 'Italiano',
+			blocklyLang: 'Inglese',
 			drawer: null,
 			source: null,
 			msg: 'Welcome to Your Vue.js App',
