efa11c0e68ee9c578fc5bbffc48c768229c19866
Antonio Vivace <avivace4@gmail.com>
2018-10-16 10:48:16 +0200

    Inject sidebar, status card, improvements

--- src/components/Settings.vue
+++ src/components/Settings.vue
@@ -1,71 +1,9 @@
 <template>
 	<div>
 		<v-app id="inspire">
-			<v-navigation-drawer fixed v-model="drawer" app>
-				<v-list dense>
-					<v-list-tile>
-						<v-list-tile-action>
-							<v-icon>home</v-icon>
-						</v-list-tile-action>
-						<v-list-tile-content>
-							<v-list-tile-title>Home</v-list-tile-title>
-						</v-list-tile-content>
-					</v-list-tile>
-					Utente
-					<v-list-tile>
-						<v-list-tile-action>
-							<v-icon>account_box</v-icon>
-						</v-list-tile-action>
-						<v-list-tile-content>
-							<v-list-tile-title>Luigi Beretta</v-list-tile-title>
-						</v-list-tile-content>
-					</v-list-tile>
-					<v-list-tile>
-						<v-list-tile-action>
-							<v-icon>exit_to_app</v-icon>
-						</v-list-tile-action>
-						<v-list-tile-content>
-							<v-list-tile-title>Logout</v-list-tile-title>
-						</v-list-tile-content>
-					</v-list-tile>
-					Attività
-					<v-list-tile>
-						<v-list-tile-action>
-							<v-icon>add</v-icon>
-						</v-list-tile-action>
-						<v-list-tile-content>
-							<v-list-tile-title>Nuova</v-list-tile-title>
-						</v-list-tile-content>
-					</v-list-tile>
-					<v-list-tile>
-						<v-list-tile-action>
-							<v-icon>open_in_new</v-icon>
-						</v-list-tile-action>
-						<v-list-tile-content>
-							<v-list-tile-title>Apri</v-list-tile-title>
-						</v-list-tile-content>
-					</v-list-tile>
-					<v-list-tile>
-						<v-list-tile-action>
-							<v-icon>close</v-icon>
-						</v-list-tile-action>
-						<v-list-tile-content>
-							<v-list-tile-title>Chiudi</v-list-tile-title>
-						</v-list-tile-content>
-					</v-list-tile>
-					Altro
-					<v-list-tile>
-						<v-list-tile-action>
-							<v-icon>settings</v-icon>
-						</v-list-tile-action>
-						<v-list-tile-content>
-							<v-list-tile-title>Impostazioni</v-list-tile-title>
-						</v-list-tile-content>
-					</v-list-tile>
-				</v-list>
-			</v-navigation-drawer>
+			<sidebar></sidebar>
 			<v-toolbar color="indigo" dark fixed app>
-				<v-toolbar-side-icon @click.stop="drawer = !drawer"></v-toolbar-side-icon>
+				<v-toolbar-side-icon @click.stop="toggleSidebar()"></v-toolbar-side-icon>
 				<v-toolbar-title>Impostazioni</v-toolbar-title>
 				<v-spacer></v-spacer>
 				<v-toolbar-items>
@@ -83,31 +21,71 @@
 			<v-content>
 				<v-tabs-items v-model="tab">
 					<v-tab-item>
-						<br>
-						<v-btn color="info">Spegni</v-btn>
-						<v-btn color="info">Riavvia</v-btn>
-						<v-btn color="warning">Aggiorna</v-btn>
-						<br><br>
-						<div v-for="(value, key, index) in cbInfo">
-							{{ key }}: <code>{{ value }}</code>
-						</div>
-						<v-text-field v-model="cbName" label="Nome CoderBot"></v-text-field>
-						<v-btn color="error">Ripristina ad Impostazioni di fabbrica</v-btn>
+						<v-container grid-list-md text-xs-center>
+							<v-layout row wrap>
+								<!-- Column A -->
+								<v-flex xs12 md6 offset-md3>
+									<h3 class="text-xs-left">Sistema </h3>
+									<v-card>
+										<div class="cardContent">
+											<v-text-field v-model="cbName" label="Nome CoderBot"></v-text-field>
+											<br>
+											<div v-for="(value, key, index) in cbInfo">
+												{{ key }}: <code>{{ value }}</code>
+											</div>
+										</div>
+									</v-card>
+									<br>
+									<h3 class="text-xs-left">Stato </h3>
+									<v-card>
+										<br>
+										Robe
+										<br>
+										<br>
+									</v-card>
+									<br>
+									<h3 class="text-xs-left"> Azioni </h3>
+									<v-card>
+										<div class="cardContent">
+											<v-btn color="info">Spegni</v-btn>
+											<v-btn color="info">Riavvia</v-btn>
+											<v-btn color="warning">Aggiorna</v-btn>
+											<v-btn color="error">Ripristina ad Impostazioni di fabbrica</v-btn>
+										</div>
+									</v-card>
+									<br>
+
+								</v-flex>
+							</v-layout>
+						</v-container>
 					</v-tab-item>
 					<v-tab-item>
-						<v-radio-group v-model="wifiMode" column>
-							Configurazione di Rete
-							<v-radio label="Connetti ad una rete WiFi già esistente (client)" value="wificlient"></v-radio>
-							<v-radio label="Lascia che CoderBot configuri la propria rete WiFi locale (ad-hoc)" value="wifiadhoc"></v-radio>
-							<div v-if="wifiMode == 'wificlient'">
-								<v-text-field v-model="wifiSSID" label="SSID"></v-text-field>
-								<v-text-field v-model="wifiPsw" label="Password"></v-text-field>
-							</div>
-							<div v-if="wifiMode =='wifiadhoc'"> SSID: <code> coderbot </code> Password: <code>coderb0t01 </code> </div>
-						</v-radio-group>
+						<v-container grid-list-md text-xs-center>
+							<v-layout row wrap>
+								<!-- Column A -->
+								<v-flex xs12 md6 offset-md3>
+									<h3 class="text-xs-left">Configurazione di Rete</h3>
+									<v-card>
+										<div class="cardContent">
+											<v-radio-group v-model="wifiMode" column>
+												<v-radio label="Connetti ad una rete WiFi già esistente (client)" value="wificlient"></v-radio>
+												<v-radio label="Lascia che CoderBot configuri la propria rete WiFi locale (ad-hoc)" value="wifiadhoc"></v-radio>
+												<div v-if="wifiMode == 'wificlient'">
+													<v-text-field v-model="wifiSSID" label="SSID"></v-text-field>
+													<v-text-field v-model="wifiPsw" label="Password"></v-text-field>
+												</div>
+												<div v-if="wifiMode =='wifiadhoc'"> SSID: <code> coderbot </code> Password: <code>coderb0t01 </code> </div>
+											</v-radio-group>
+										</div>
+									</v-card>
+								</v-flex>
+							</v-layout>
+						</v-container>
+						<v-card-text>
+						</v-card-text>
 					</v-tab-item>
 					<v-tab-item>
-						<wsFactory></wsFactory>
+						Tab 3
 					</v-tab-item>
 					<v-tab-item>
 						Tab 4
@@ -118,10 +96,17 @@
 	</div>
 </template>
 <script>
-import wsFactory from '../components/wsFactory';
+import sidebar from "../components/Sidebar"
 
 export default {
+	components: { sidebar },
 	name: 'HelloWorld',
+	methods: {
+		toggleSidebar: function() {
+			let currentStatus = this.$store.getters.drawerStatus
+			this.$store.commit('toggleDrawer', !currentStatus)
+		}
+	},
 	data() {
 		return {
 			cbName: 'CoderBot di Antonio',
@@ -141,3 +126,9 @@ export default {
 }
 
 </script>
+<style scoped>
+.cardContent {
+	padding: 16px;
+}
+
+</style>
