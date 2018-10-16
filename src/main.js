a11df651810542604345f2e00e67dcb7049d90a7
Antonio Vivace <avivace4@gmail.com>
2018-10-16 17:41:39 +0200

    Router middleware: set document.title based on title meta value in route definition

--- src/main.js
+++ src/main.js
@@ -41,18 +41,33 @@ Vue.config.productionTip = false;
 
 // Routing
 const routes = [
-  { path: '/', component: landing },
-  { path: '/run', component: run },
-  { path: '/ar', component: arexample },
-  { path: '/blockly', component: blockly },
-  { path: '/new', component: activityEditor },
-  { path: '/settings', component: settings },
+  { path: '/', component: landing, meta: {title: 'CoderBot'} },
+  { path: '/run', component: run, meta: {title: 'CoderBot'} },
+  { path: '/ar', component: arexample, meta: {title: 'CoderBot'} },
+  { path: '/blockly', component: blockly, meta: {title: 'CoderBot - Blockly'}},
+  { path: '/new', component: activityEditor, meta: {title: 'CoderBot - Nuova Attività'}},
+  { path: '/settings', component: settings , meta: {title: 'CoderBot - Impostazioni'}},
 ];
 
+
 const router = new VueRouter({
-  routes, // short for `routes: routes`
+  routes, // short for `routes: routes` (ES6)
 });
 
+// This callback runs before every route change, including on page load.
+router.beforeEach((to, from, next) => {
+  // This goes through the matched routes from last to first, finding the closest route with a title.
+  // eg. if we have /some/deep/nested/route and /some, /deep, and /nested have titles, nested's will be chosen.
+  const nearestWithTitle = to.matched.slice().reverse().find(r => r.meta && r.meta.title);
+
+  // Find the nearest route element with meta tags.
+  const nearestWithMeta = to.matched.slice().reverse().find(r => r.meta && r.meta.metaTags);
+
+  // If a route with a title was found, set the document (page) title to that value.
+  if(nearestWithTitle) document.title = nearestWithTitle.meta.title;
+
+  next();
+});
 
 /* eslint-disable no-new */
 new Vue({
