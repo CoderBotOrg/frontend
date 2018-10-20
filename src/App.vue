3e39b7243a783f6cfa92fd9d839b88153a095ffe
Antonio Vivace <avivace4@gmail.com>
2018-10-20 20:10:24 +0200

    Remove font styling

--- src/App.vue
+++ src/App.vue
@@ -17,12 +17,10 @@ export default {
 </script>
 <style>
 #app {
-	font-family: 'Avenir', Helvetica, Arial, sans-serif;
 	-webkit-font-smoothing: antialiased;
 	-moz-osx-font-smoothing: grayscale;
 	text-align: center;
 	color: #2c3e50;
-	font-weight: 500;
 }
 
 </style>
