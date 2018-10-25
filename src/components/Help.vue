95a70b5c61aaf53284d5ac59fb3131be8c9f948c
Antonio Vivace <avivace4@gmail.com>
2018-10-25 11:36:57 +0200

    Empty components

---
+++ src/components/Help.vue
@@ -0,0 +1,32 @@
+<template>
+	<div>
+		<v-app id="inspire">
+			<sidebar></sidebar>
+						<v-toolbar color="indigo" dark fixed app>
+				<v-toolbar-side-icon @click.stop="toggleSidebar()"></v-toolbar-side-icon>
+				<v-toolbar-title>Aiuto</v-toolbar-title>
+			</v-toolbar>
+			<v-content>Placeholder content</v-content>
+		</v-app>
+	</div>
+</template>
+<script>
+import sidebar from "../components/Sidebar"
+
+export default {
+	components: { sidebar },
+	name: 'HelloWorld',
+	methods: {
+		toggleSidebar: function() {
+			let currentStatus = this.$store.getters.drawerStatus
+			this.$store.commit('toggleDrawer', !currentStatus)
+		}
+	},
+	data() {
+		return {
+			drawer: null,
+		};
+	},
+};
+
+</script>
