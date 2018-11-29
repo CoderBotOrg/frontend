507c8b5964107b8667afd6b2f9b7151b968703bf
Antonio Vivace <avivace4@gmail.com>
2018-11-29 09:41:48 +0100

    add template component

---
+++ src/components/.template.vue
@@ -0,0 +1,36 @@
+<template>
+	<div>
+		<v-app id="inspire">
+			<sidebar></sidebar>
+			<v-toolbar color="indigo" dark fixed app>
+				<v-toolbar-side-icon @click.stop="toggleSidebar()"></v-toolbar-side-icon>
+				<v-toolbar-title>CoderBot</v-toolbar-title>
+			</v-toolbar>
+			<v-content>
+				<!-- Content -->
+			</v-content>
+		</v-app>
+	</div>
+</template>
+<script>
+import sidebar from "../components/Sidebar"
+
+export default {
+	components: { sidebar },
+	name: 'CoderBot',
+	methods: {
+		toggleSidebar: function() {
+			let currentStatus = this.$store.getters.drawerStatus
+			this.$store.commit('toggleDrawer', !currentStatus)
+		}
+	},
+	data() {
+		return {
+			drawer: null
+		};
+	},
+};
+</script>
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+</style>
