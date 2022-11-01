bc923375a6c6be3c1762b6ccadcad49b9e5054a5
previ <roberto.previtera@gmail.com>
2022-11-01 10:47:49 +0100

    e2e tests

--- cypress/e2e/200_docs.cy.js
+++ cypress/e2e/200_docs.cy.js
@@ -2,8 +2,8 @@ describe('load homepage', () => {
   it('checks sidebar contents', () => {
     cy.visit('http://localhost:8080')
     cy.get('button.v-app-bar-nav-icon').should('exist').click()
-    cy.get('a[href*="/docs"]').should('exist')
-    cy.get('a[href*="/docs"]').invoke('removeAttr', 'target').click()
+    cy.get('a.v-list-item[href*="/docs"]').should('exist')
+    cy.get('a.v-list-item[href*="/docs"]').invoke('removeAttr', 'target').click()
     cy.url().should('include', '/docs')
   })
 })
--- cypress/e2e/600_gallery.cy.js
+++ cypress/e2e/600_gallery.cy.js
@@ -2,7 +2,8 @@ describe('load homepage', () => {
   it('checks sidebar contents', () => {
     cy.visit('http://localhost:8080')
     cy.get('button.v-app-bar-nav-icon').should('exist').click()
-    cy.get.v-list-item('a[href*="/gallery"]').click()
+    cy.get('a.v-list-item[href*="/gallery"]').should('exist')
+    cy.get('a.v-list-item[href*="/gallery"]').click()
     cy.get('h3').should('exist')
   })
 })
