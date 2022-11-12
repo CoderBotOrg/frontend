5b80003258f7a6d6b4c8cd072a1ffce04979f5ed
previ <roberto.previtera@gmail.com>
2022-11-13 00:09:36 +0100

    e2e tests

--- cypress/e2e/500_control.cy.js
+++ cypress/e2e/500_control.cy.js
@@ -1,4 +1,4 @@
-describe('load homepage', () => {
+describe('load control page, verify stream is there', () => {
   it('checks sidebar contents', () => {
     cy.visit('http://localhost:8080')
     cy.get('.v-carousel').should('exist')
@@ -7,15 +7,55 @@ describe('load homepage', () => {
     cy.get('img[src*="video/stream"]').should('exist')
   })
 
-  it('takes picture', () => {
+  it('move', () => {
+    cy.visit('http://localhost:8080')
+    cy.get('.v-carousel').should('exist')
+    cy.get('button.v-app-bar-nav-icon').should('exist').click()  
+    cy.get('a.v-list-item[href*="control"]').click()
+    cy.get('#app').click()
+    cy.get('button.up').click()
+    cy.get('button.left').click()
+    cy.get('button.right').click()
+    cy.get('button.down').click()
   })
 
-  it('say a phrase', () => {
+  it('takes a picture', () => {
+    cy.visit('http://localhost:8080')
+    cy.get('.v-carousel').should('exist')
+    cy.get('button.v-app-bar-nav-icon').should('exist').click()  
+    cy.get('a.v-list-item[href*="control"]').click()
+    cy.get('#app').click()
+    cy.get('button.photo').click()
   })
 
   it('record a video', () => {
+    cy.visit('http://localhost:8080')
+    cy.get('.v-carousel').should('exist')
+    cy.get('button.v-app-bar-nav-icon').should('exist').click()  
+    cy.get('a.v-list-item[href*="control"]').click()
+    cy.get('#app').click()
+    cy.get('button.video').click()
+    cy.get('button.video').click()
+  })
+
+  it('say a phrase', () => {
+    cy.visit('http://localhost:8080')
+    cy.get('.v-carousel').should('exist')
+    cy.get('button.v-app-bar-nav-icon').should('exist').click()  
+    cy.get('a.v-list-item[href*="control"]').click()
+    cy.get('#app').click()
+    cy.get('button.speak').click()
+    cy.get('#control_text_to_speech').clear().type('hello world!')
+    cy.get('#dialog_control_speak_title').find('button.ok').click()
   })
 
   it('opens gallery', () => {
+    cy.visit('http://localhost:8080')
+    cy.get('.v-carousel').should('exist')
+    cy.get('button.v-app-bar-nav-icon').should('exist').click()  
+    cy.get('a.v-list-item[href*="control"]').click()
+    cy.get('#app').click()
+    cy.get('a.gallery').click()
+    cy.url().should('include', '/gallery')
   })
 })
