0858039e4745306fc38503abbce998849d6eaa21
previ <roberto.previtera@gmail.com>
2023-04-27 01:56:50 +0200

    wip

--- src/components/Sync.vue
+++ src/components/Sync.vue
@@ -82,7 +82,7 @@ export default {
   data() {
     return {
       syncModes: this.syncModesInit,
-      syncPeriod: this.syncPeriodInit
+      syncPeriod: this.syncPeriodInit,
     };
   },
   validations() {
