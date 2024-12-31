dcb4aaf6549d944d91811970de26162573a5b1a0
previ <roberto.previtera@gmail.com>
2024-12-31 17:14:22 +0100

    fix gallery test

--- cypress/e2e/500_control.cy.js
+++ cypress/e2e/500_control.cy.js
@@ -26,6 +26,17 @@ describe('load control page, verify stream is there', () => {
     cy.get('a.v-list-item[href*="control"]').click()
     cy.get('#app').click()
     cy.get('button.photo').click()
+    cy.visit('http://localhost:8080')
+    cy.get('.v-carousel').should('exist')
+    cy.get('button.v-app-bar-nav-icon').should('exist').click()  
+    cy.get('a.v-list-item[href*="gallery"]').click()
+    cy.get('#app').click()
+    cy.get('.gallery').contains('DSC1').should('exist')
+    cy.get('.v-main').contains('DSC1').click()
+    cy.get('.v-card-title.details').contains('DSC1').should('exist')
+    cy.get('.v-card-title.details').contains('DSC1').parents('.v-card').find('button.ok').click()
+    cy.get('.v-main').contains('DSC1').parents('.v-card').find('.mdi-delete').click()
+    cy.get('#gallery_photo_confirm_card').find('.ok').click()
   })
 
   it('record a video', () => {
@@ -37,6 +48,18 @@ describe('load control page, verify stream is there', () => {
     cy.get('button.video').click()
     cy.wait(5000)
     cy.get('button.video').click()
+    cy.visit('http://localhost:8080')
+    cy.get('.v-carousel').should('exist')
+    cy.get('button.v-app-bar-nav-icon').should('exist').click()  
+    cy.get('a.v-list-item[href*="gallery"]').click()
+    cy.get('#app').click()
+    cy.get('.gallery').contains('VID1').should('exist')
+    cy.get('.v-main').contains('VID1').click()
+    cy.get('.v-card-title.details').contains('VID1').should('exist')
+    cy.get('.v-card-title.details').contains('VID1').parents('.v-card').find('button.ok').click()
+    cy.get('.v-main').contains('VID1').parents('.v-card').find('.mdi-delete').click()
+    cy.get('#gallery_photo_confirm_card').find('.ok').click()
+
   })
   /*
   it('say a phrase', () => {
@@ -59,5 +82,5 @@ describe('load control page, verify stream is there', () => {
     cy.get('a.gallery').click()
     cy.url().should('include', '/gallery')
   })
-  */
+  */  
 })
